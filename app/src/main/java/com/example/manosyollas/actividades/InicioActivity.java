919ca14d6ddcc43fa2541f9d5a56ad29966b5428
Nayeli Chuquipul <naych.gon@gmail.com>
2024-08-30 13:26:55 -0500

    Pantalla de Inicio
    Cada boton ya esta enlazado a otras pantallas en blanco

--- app/src/main/java/com/example/manosyollas/actividades/InicioActivity.java
+++ app/src/main/java/com/example/manosyollas/actividades/InicioActivity.java
@@ -1,6 +1,9 @@
 package com.example.manosyollas.actividades;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +13,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.manosyollas.R;
 
-public class InicioActivity extends AppCompatActivity {
-
+public class InicioActivity extends AppCompatActivity implements View.OnClickListener {
+Button btnIngresar, btnRegistrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +25,31 @@ public class InicioActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnIngresar = findViewById(R.id.btnIngresar);
+        btnRegistrar = findViewById(R.id.btnRegistrate);
+
+        btnIngresar.setOnClickListener(this);
+        btnRegistrar.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId()== R.id.btnIngresar)
+            ingresar();
+        else if(v.getId() == R.id.btnRegistrate)
+            registrate();
+    }
+
+    private void registrate() {
+        Intent registro = new Intent(this, RegistrateActivity.class);
+        startActivity(registro);
+        finish();
+    }
+
+    private void ingresar() {
+        Intent ingresar = new Intent(this, LoginActivity.class);
+        startActivity(ingresar);
+        finish();
     }
 }
