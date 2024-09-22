2ea9258db4ad6b98832eed5683124f097dd5b632
Liz <liz.asenjo123@gmail.com>
2024-09-21 20:17:57 -0500

    Fragmentos

--- app/src/main/java/com/example/manosyollas/actividades/TransferenciaActivity.java
+++ app/src/main/java/com/example/manosyollas/actividades/TransferenciaActivity.java
@@ -12,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.manosyollas.R;
+import com.example.manosyollas.fragmentos.DonacionesFragment;
 
 public class TransferenciaActivity extends AppCompatActivity {
 
@@ -30,7 +31,7 @@ public class TransferenciaActivity extends AppCompatActivity {
         icFlotante.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(TransferenciaActivity.this, DonacionesActivity.class);
+                Intent intent = new Intent(TransferenciaActivity.this, DonacionesFragment.class);
                 startActivity(intent);
                 finish();
             }
