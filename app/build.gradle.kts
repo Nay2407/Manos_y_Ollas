762f7133c663ef105857edd4af343003577de1d6
Fabricio-Franco-Iman-Bonilla <brick9201@gmail.com>
2024-11-03 21:43:07 -0500

    Gugul Base de Fuego

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,6 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:23.1.0")
     implementation("com.google.android.gms:play-services-auth:21.2.0")
 
-
     implementation("com.loopj.android:android-async-http:1.4.9")
     implementation("com.android.volley:volley:1.2.1")
     implementation ("org.osmdroid:osmdroid-android:6.1.10")
